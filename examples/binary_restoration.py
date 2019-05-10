766fa5ac64822c9845b2a04f445ef0fb2c23cbd1
Pablo Márquez Neila <pmarquezneila@gmail.com>
2019-05-10 02:13:15 +0200

    Fixed bug in the wrapper for get_first_arc
    It was giving problems in Windows 10 when get_nx_graph was called

--- examples/binary_restoration.py
+++ examples/binary_restoration.py
@@ -1,7 +1,6 @@
 
 import numpy as np
-import scipy
-from scipy.misc import imread
+from imageio import imread
 from matplotlib import pyplot as ppl
 
 import maxflow
@@ -16,8 +15,6 @@ nodeids = g.add_grid_nodes(img.shape)
 g.add_grid_edges(nodeids, 50)
 # Add the terminal edges.
 g.add_grid_tedges(nodeids, img, 255-img)
-
-graph = g.get_nx_graph()
 
 # Find the maximum flow.
 g.maxflow()
